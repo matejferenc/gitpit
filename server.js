795fb197510cb1f73b94497f1add146574e1eeb1
Matej Ferenc <matej.ferenc@etnetera.cz>
2013-09-11 12:34:55 +0200

    server in own module

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var http = require("http");
 
-function onRequest(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+
+function start() {
+  function onRequest(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
+  
+  http.createServer(onRequest).listen(8888);
+  
+  console.log("server started")
+
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log("server started")
+exports.start = start;
