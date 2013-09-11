12223a3955d426a92c905109d37545dc14c6ca68
Matej Ferenc <matej.ferenc@etnetera.cz>
2013-09-11 13:48:30 +0200

    writing response in handlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,22 @@
-function start() {
+function start(response) {
   console.log("Request handler 'start' was called.");
+  
+  function sleep(milliSeconds) {
+    var startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + milliSeconds);
+  }
+  
+  sleep(10000);
+  
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Start");
+  response.end();
 }
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
